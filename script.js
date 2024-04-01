f8ad68cbabae4629f4121ba393ec596e8cbf5ec0
parvaj420 <parvez1a2b9c@gmail.com>
2024-04-01 12:39:02 +0600

    other item added

--- script.js
+++ script.js
@@ -118,6 +118,296 @@ function index(ind) {
 index(0)
 index(1)
 index(2)
+// SECTION 1
+
+
+
+
+function section1(card_image_top, card_image_alt = "Hello, i'm sorry", title, text, link, category, color, type, cardno) {
+
+          let main = document.querySelector(".section1 .col-md-9 .d-flex")
+  
+          let card = document.createElement("div")
+          let card_image_div = document.createElement("div") 
+          let card_img_top = document.createElement("img")
+          let card_body = document.createElement("div")
+          let card_title = document.createElement("h5")
+          let card_text = document.createElement("p")
+          let card_body2 = document.createElement("div")
+          let card_link = document.createElement("a")
+
+              card.className = "card"
+              card.classList.add(cardno)
+              // [category,color,type]
+              card_img_top.className = "card-img-top"
+              card_img_top.classList.add("image")
+              card_img_top.classList.add("cap")
+              card_body.className = "card-body"
+              card_title.className = "card-title"
+              card_text.className = "card-text"
+              card_body2.className = "card-body"
+              card_link.className = "card-link"
+              card_link.classList.add("stretched-link")
+              // card_link.classList.add()
+
+              card_img_top.setAttribute("src", card_image_top);
+              card_img_top.setAttribute("alt", card_image_alt);
+              card_image_div.className = "card-img-div"
+              card_image_div.appendChild(card_img_top)
+
+              card_title.innerHTML = title
+              card_text.innerHTML = text
+              card_body.appendChild(card_title)
+              card_body.appendChild(card_text)
+
+              card_link.setAttribute("href", link)
+              card_link.setAttribute("target", "_blank")
+              card_link.className["card-link", "stretched-link"]
+              card_body2.appendChild(card_link)
+              card_link.innerHTML = "Explore page"
+
+              card.appendChild(card_image_div)
+              card.appendChild(card_body)
+              card.appendChild(card_body2)
+
+      main.appendChild(card)
+}
+
+
+        section1("img/1.jpg", "", "e-Commerce web design", "Sleek, intuitive e-commerce web design with seamless user experience.", "https://e3-five.vercel.app/component", "ecomm", "color1", "landing", "card1")
+        section1("img/2.jpg", "", "e-Commerce Shop Website Template", "Sleek, intuitive e-commerce web design with seamless user experience.", "https://ecommerce5-five.vercel.app/", "ecomm", "color2", "landing", "card2")
+        section1("img/3.jpg","", "Minimal Blog Website Template", "Engage users with minimalist design, clear navigation, and compelling content snippets.", "https://whoprogrammer.github.io/4/", "blog", "color3", "hero", "card3")
+        section1("img/4.jpg", "", "Beauty & Wellness eCommerce Landing...", "Beauty & wellness eCommerce Website Template design.", "https://parvaj420.github.io/ecommerce-webpage-design.com/", "ecomm", "color4", "landing", "card4")
+        section1("img/5.jpg", "", "Modern hero section design.", "Beauty and Wellness eCommerce Landing Page Design", "https://whoprogrammer.github.io/1/", "blog", "color3", "hero", "card5")
+        section1("img/6.jpg", "", "Modern hero section design", "Beauty and Wellness eCommerce Landing Page Design", "https://whoprogrammer.github.io/2/", "ecomm", "color2", "hero", "card6")
+        section1("img/7.jpg", "", "Portfolio HERO section design.", "My landing page template is designed for user portfolio.", "https://whoprogrammer.github.io/3/", "port", "color3", "hero", "card7")
+        section1("img/8.jpg", "", "Fashion eCommerce shop website template", "My fashion eCommerce website template design.", "https://parvaj420.github.io/e2/", "ecomm", "color4", "landing", "card8")
+        section1("img/9.jpg", "", "Restaurant landing page", "My Premium Restaurant Landing Page is designed for the modern restaurateur looking for a great website.", "https://parvaj420.github.io/www.food-website.com/", "resto", "color1", "landing", "card9")
+        section1("img/10.jpg", "", "Portfolio design", "simple portfolio landing Page design.", "https://parvaj420.github.io/portfolio-website-design.com/", "port", "color2", "landing", "card10")
+        section1("img/11.jpg", "", "Construction & Contractors Website Template", "Experience superior design with our industry-leading Construction Website Template.", "https://parvaj420.github.io/ecommerce-website-design/", "ecomm", "color4", "landing", "card11")
+        section1("img/12.jpg", "", "Landing page design", "Clean, intuitive landing page with minimalist design to engage users effectively.", "https://parvaj420.github.io/website-design.com/#", "", "color1", "landing", "card12")
+        section1("img/14.jpg", "", "Hero category for games advertising", "User-friendly web interface for analyzing game data with interactive visualizations.", "https://whoprogrammer.github.io/6/", "game", "color2", "hero", "card13")
+
+  function resat() {
+
+    function all(display) {
+      let e = document.querySelectorAll(".section1 .col-md-9 .card")
+          e.forEach((ev) => {
+            // ev.classList.remove(remove)
+            // ev.classList.add(display)
+            ev.style.display = display
+          })
+    }
+    function card(cardNo, display) {
+      let cardName = document.querySelector(`.section1 .col-md-9 .card${cardNo}`)
+          cardName.style.display = (display)
+          // cardName.classList.remove(rem)
+    }
+  
+
+
+    let resat1 = document.querySelector(".section1 .col-md-3 .resat")
+
+    let ecomm1 = document.querySelector(".section1 .col-md-3 .ecomm")
+    let blog1 = document.querySelector(".section1 .col-md-3 .blog")
+    let port1= document.querySelector(".section1 .col-md-3 .port")
+    let rest1 = document.querySelector(".section1 .col-md-3 .rest")
+    let game1= document.querySelector(".section1 .col-md-3 .game")
+
+    let Color1 = document.querySelector(".section1 .col-md-3 .color1")
+    let Color2 = document.querySelector(".section1 .col-md-3 .color2")
+    let Color3 = document.querySelector(".section1 .col-md-3 .color3")
+    let Color4 = document.querySelector(".section1 .col-md-3 .color4")
+    
+    let Hero_site = document.querySelector(".section1 .col-md-3 .hero-site")
+    let Landing_site = document.querySelector(".section1 .col-md-3 .landing-site")
+
+        resat1.addEventListener("click", () => {
+          all("block")
+        })
+
+          ecomm1.addEventListener("click", () => {
+          card("1", "block")
+          card("2", "block")
+          card("4", "block")
+          card("6", "block")
+          card("8", "block")
+          card("11", "block")
+
+          card("3", "none")
+          card("5", "none")
+          card("7", "none")
+          card("9", "none")
+          card("10", "none")
+          card("12", "none")
+          card("13", "none")
+        })
+        blog1.addEventListener("click", () => {
+          card(3, "block")
+          card(5, "block")
+
+          card(1, "none")
+          card(2, "none")
+          card(4, "none")
+          card(6, "none")
+          card(7, "none")
+          card(8, "none")
+          card(9, "none")
+          card(10, "none")
+          card(11, "none")
+          card(12, "none")
+          card(13, "none")
+        })
+        port1.addEventListener("click", () => {
+          card(10, "block")
+          card(7, "block")
+
+          card(3, "none")
+          card(4, "none")
+          card(5, "none")
+          card(1, "none")
+          card(2, "none")
+          card(8, "none")
+          card(9, "none")
+          card(6, "none")
+          card(11, "none")
+          card(12, "none")
+          card(13, "none")
+        })
+        rest1.addEventListener("click", () => {
+          card(9, "block")
+
+          card(3, "none")
+          card(8, "none")
+          card(4, "none")
+          card(5, "none")
+          card(6, "none")
+          card(7, "none")
+          card(1, "none")
+          card(2, "none")
+          card(10, "none")
+          card(11, "none")
+          card(12, "none")
+          card(13, "none")
+        })
+        game1.addEventListener("click", () => {
+          card(13, "block")
+
+          card(3, "none")
+          card(4, "none")
+          card(5, "none")
+          card(6, "none")
+          card(7, "none")
+          card(8, "none")
+          card(9, "none")
+          card(10, "none")
+          card(1, "none")
+          card(11, "none")
+          card(2, "none")
+          card(12, "none")
+        })
+
+        Color1.addEventListener("click", () => {
+          card(1, "block")
+          card(9, "block")
+          card(12, "block")
+
+          card(2, "none")
+          card(3, "none")
+          card(4, "none")
+          card(5, "none")
+          card(6, "none")
+          card(7, "none")
+          card(8, "none")
+          card(10, "none")
+          card(11, "none")
+          card(13, "none")
+        })
+        Color2.addEventListener("click", () => {
+          card(2, "block")
+          card(6, "block")
+          card(10, "block")
+          card(13, "block")
+
+          card(1, "none")
+          card(9, "none")
+          card(12, "none")
+          card(3, "none")
+          card(4, "none")
+          card(5, "none")
+          card(7, "none")
+          card(8, "none")
+          card(11, "none")
+        })
+        Color3.addEventListener("click", () => {
+          card(3, "block")
+          card(5, "block")
+          card(7, "block")
+
+          card(2, "none")
+          card(4, "none")
+          card(6, "none")
+          card(1, "none")
+          card(9, "none")
+          card(12, "none")
+          card(8, "none")
+          card(10, "none")
+          card(11, "none")
+          card(13, "none")
+        })
+        Color4.addEventListener("click", () => {
+          card(4, "block")
+          card(8, "block")
+          card(11, "block")
+
+          card(1, "none")
+          card(9, "none")
+          card(12, "none")
+          card(2, "none")
+          card(3, "none")
+          card(5, "none")
+          card(6, "none")
+          card(7, "none")
+          card(10, "none")
+          card(13, "none")
+        })
+
+        Hero_site.addEventListener("click", () => {
+          card(3, "block")
+          card(5, "block")
+          card(6, "block")
+          card(7, "block")
+          card(13, "block")
+
+          card(1, "none")
+          card(2, "none")
+          card(4, "none")
+          card(8, "none")
+          card(9, "none")
+          card(10, "none")
+          card(11, "none")
+          card(12, "none")
+        })
+        Landing_site.addEventListener("click", () => {
+          card(3, "none")
+          card(5, "none")
+          card(6, "none")
+          card(7, "none")
+          card(13, "none")
+
+          card(1, "block")
+          card(2, "block")
+          card(4, "block") 
+          card(8, "block")
+          card(9, "block")
+          card(10, "block")
+          card(11, "block")
+          card(12, "block")
+        })
+
+  }
+  resat()
+ 
 
 // SECTION 2
 
